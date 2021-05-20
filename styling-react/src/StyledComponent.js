bc3b00d7c672c3bee233418f3be8feaaa345e66c
yoosun96 <yoosnu@naver.com>
2021-05-20 21:30:25 +0900

    JS: StuledComponents

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
+const sizes = {
+    desktop: 1024,
+    tablet: 768
+};
+
+//위에있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어 줍니다.
+const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em){
+        ${css(...args)}
+    }
+    `;
+    return acc;
+}, {});
+
 const Box = styled.div`
 background: ${props => props.color || 'blue'};
 padding: 1rem;
 display: flex;
+width: 1024px;
+margin: 0 auto;
+${media.desktop`width:768px;`}
+${media.tablet`width:100%;`};
 `;
 
 const Button = styled.button`
@@ -21,7 +40,6 @@ font-weight:600;
 &:hover{
     backgound: rgba(255,255,255,0.9);
 }
-
 ${props =>
         props.inverted &&
         css`
@@ -48,4 +66,4 @@ const StyledComponent = () => {
     )
 }
 
-export default StyledComponent; 
+export default StyledComponent;
