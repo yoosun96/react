d5712d9355f9deb0de8d3a26d3eb586d928f0a30
yoosun96 <yoosnu@naver.com>
2021-06-17 19:46:51 +0900

    commit

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,56 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
 import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
-box-sizing: border-box;
-padding-bottom: 3rem;
-width: 768px;
-margin: 0 auto;
-margin-top: 2rem;
-@media screen and (max-width: 768px){
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
     width: 100%;
     padding-left: 1rem;
     padding-right: 1rem;
-}
+  }
 `;
 
-
 const NewsList = ({ category }) => {
-    const [loading, response, error] = usePromise(() => {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        return axios.get(
-            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c6fd80410db84cb3befea3e8843722dd`,
-        );
-    }, [category]);
-
-
-
-    // 대기 중일떄
-    if (loading) {
-        return <NewsListBlock>대기 중...</NewsListBlock>;
-    }
-    // 아직 response 값이 설정되지 않았을 때
-    if (!response) {
-        return null;
-    }
-    // 에러가 발생했을 때
-    if (error) {
-        return <NewsListBlock>에러 발생</NewsListBlock>
-    }
-
-
-    // response 값이 유요할 때
-    const { articles } = response.data;
-    return (
-        <NewsListBlock>
-            {articles.map(article => (
-                <NewsItem key={article.url} article={article} />
-            ))}
-        </NewsListBlock>
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c6fd80410db84cb3befea3e8843722dd`,
     );
+  }, [category]);
+
+  // 대기 중일떄
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
+    return null;
+  }
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생</NewsListBlock>;
+  }
+
+  // response 값이 유요할 때
+  const { articles } = response.data;
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
 };
 
 export default NewsList;
